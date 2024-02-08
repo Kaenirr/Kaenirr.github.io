98f21dab055a9f2471119c598f42e8d26f1d64e1
HorM <horm@trisgram.com>
2024-02-08 16:51:51 +0100

    Little text changes

--- src/routes/aboutMe/+page.svelte
+++ src/routes/aboutMe/+page.svelte
@@ -1,3 +1,4 @@
 <h1>About Me:</h1>
 <p>I am Martin, a 3D artist/programmer consumed by my desire to learn everything</p>
 <p>This page is an remainder and one of my sources of motivation to continue working on things I love and expanding my horizons</p>
+<p>Also it's my way to learn svelte and web development properly from the basics</p>
