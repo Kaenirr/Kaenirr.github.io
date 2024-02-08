5e1beff0239a52abaf32206721305e9412a5829e
HorM <horm@trisgram.com>
2024-02-08 17:08:29 +0100

    no message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to my page!</h1>
 <p>This page is very much in progress but I am happy to welcome you here</p>
-<p>This page is going to contain my work , thoughts and other creations of my.</p>
-<p>This is my way to learn svelte and web development properly from the basics</p>
+<p>This page is going to contain my work , thoughts and other creations of mine.</p>
