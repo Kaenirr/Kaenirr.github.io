60828e14e575bc3846707fbd52223c67e0071bcf
HorM <horm@trisgram.com>
2024-04-06 10:26:18 +0200

    Updates

---
+++ _app/immutable/chunks/skills.z5A2nSF3.js
@@ -0,0 +1,96 @@
+import{t as m}from"./theme.MjmL59CL.js";import{b as h}from"./paths.3d6tUxev.js";const g=n=>`${h}/logos/${n}`,e=(n,t)=>t?{dark:g(t),light:g(n)}:g(n),a={AWS:e("aws.svg"),Bootstrap:e("bootstrap.svg"),Blender:e("blender.svg"),C:e("c.svg"),Cpp:e("cpp.svg"),Celery:e("celery.svg"),Django:e("django.svg"),FastApi:e("fastapi"),Flask:e("flask.svg"),Go:e("go.svg"),Kafka:e("kafka.svg"),Neo4j:e("neo4j.svg"),Nginx:e("nginx.svg"),Numpy:e("numpy.svg"),Pandas:e("pandas.svg"),RabbitMQ:e("rabbitmq.svg"),Rust:e("rust.svg","rust-dark.png"),Scrapy:e("scrapy.png"),Selenium:e("selenium.svg"),Docker:e("docker.svg"),Kubernetes:e("kubernetees.svg"),Csharp:e("csharp.svg"),Xamarin:e("xamarin.svg"),TypeScript:e("ts.png"),VueJs:e("vue.png"),ReactJs:e("react.svg"),Dart:e("dart.png"),Kotlin:e("kotlin.png"),Python:e("python.png"),NodeJs:e("node.png"),Deno:e("deno.png","deno-dark.png"),Svelte:e("svelte.png"),ExpressJs:e("express.png"),JavaScript:e("js.png"),Fastify:e("fastify.svg","fastify-dark.png"),NestJs:e("nest.svg"),Quasar:e("quasar.svg"),SolidJs:e("solid.svg"),Electron:e("electron.png"),Flutter:e("flutter.svg"),Java:e("java.png"),AdonisJs:e("adonis.png"),Android:e("android.png"),Angular:e("angular.png"),PostgreSQL:e("postgres.png"),Firebase:e("firebase.png"),Sass:e("sass.png"),Unknown:e("no-img.svg"),MongoDB:e("mongodb.svg"),Redis:e("redis.svg"),Tailwind:e("tailwind.svg"),HTML:e("html.svg"),Premiere:e("premiere.svg"),Photoshop:e("photoshop.svg"),CSS:e("css.svg"),AfterEffects:e("after-effects.svg"),Illustrator:e("illustrator.svg"),Nuxt:e("nuxt.png"),Vite:e("vite.png"),SubstancePainter:e("substancePainter.svg"),Unreal:e("unreal.svg"),Unity:e("unity.svg"),Vitest:e("vitest.svg"),Jest:e("jest.png"),Trisgram:e("trisgram.avif"),Unocss:e("unocss.svg"),Ruvy:e("ruvy.svg"),Postcss:e("postcss.svg"),PhotonQuantum:e("photon.svg"),PhotonFusion:e("photon.svg")};let c;m.subscribe(n=>c=n);const F=n=>typeof n=="string"?n:c?n.dark:n.light,v=(n,...t)=>{const o={...n};return Object.keys(o).forEach(r=>{t.includes(r)&&delete o[r]}),o},f=`# Blender\r
+\r
+---\r
+\r
+[\`Blender\`](https://www.blender.org/) is the free and open source 3D creation suite. It supports the entirety of the 3D pipeline—modeling, rigging, animation, simulation, rendering, compositing and motion tracking, even video editing and game creation. Advanced users employ Blender’s API for Python scripting to customize the application and write specialized tools; often these are included in Blender’s future releases. Blender is well suited to individuals and small studios who benefit from its unified pipeline and responsive development process.\r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|Blender fundamentals	|Very advanced	| Strongly familiar with most of the tools and modifiers available in blender, knowledge of high-low poly workflow	|\r
+|Hardsurface modelling	|Advanced	|Familiar with most of the bool tools and good practices for this workflow	|\r
+|Sculpting	|Beginner	|Simple understanding of tools and their usage	|\r
+|Texturing	|Advanced	|Can use nodes for procedural texturing repeatable materials, bake and prepare for usage in game engines(PBR). Profficient in UV mapping, can prepare models for usage in substance painter	|\r
+|Lighting and composition	|Beginner	|Can create simple scenes/preview renders	|\r
+|Geometry nodes	|Beginner	|Can create simple procedural models, mostly used for 3D printing. Example: parametric battery holders	|`,y=`# Substance Painter\r
+\r
+---\r
+\r
+[\`Adobe Substance 3D Painter\`](https://www.adobe.com/products/substance3d/apps/painter.html) is the reference texturing app for 3D professionals and enthusiasts. From AAA Game Developers to Indies, from Feature Animation to Visual Effects studios, the industry uses Painter to bring their creations to life thanks to its unparalleled speed, flexibility and visual quality.\r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|Fundamentals	|Beginner	| Familiar with smart materials, how to apply and change them, basic knowledge of workflow integration with game engines and blender	|\r
+`,w=`# Unity Engine\r
+\r
+---\r
+\r
+[\`Unity Engine\`](https://unity.com/) is real-time 3D development engine that lets artists, designers, and developers collaborate to create amazing immersive and interactive experiences.\r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|	|	| 	|\r
+`,b=`# Photon Fusion\r
+\r
+---\r
+\r
+[\`Photon Fusion\`](https://www.photonengine.com/fusion) is the high-end state transfer netcode SDK made for Unity Professionals. Fusion supports different network topologies and replication algorithms while exposing a single consistent API. This allows developers to focus on what really matters most: writing their own game. \r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|	|	| 	|\r
+`,k=`# Photon Quantum\r
+\r
+---\r
+\r
+[\`Photon Quantum\`](https://www.photonengine.com/quantum) is the only 100% deterministic multiplayer game engine on the market. Creating multiplayer games becomes as easy as developing a local multiplayer title: No netcode, everything is networked by default and 100% in sync. \r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|	|	| 	|\r
+`,S=`# Unreal engine 5\r
+\r
+---\r
+\r
+[\`Unreal engine 5\`](https://www.unrealengine.com/) is a video game development tool from the video game and software development company Epic Games. With this tool, developers have the ability to build a simulation, edit videos or sound, and render animations. Devs have used it to build some of the most popular games on today’s market as well.\r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|	|	| 	|\r
+`,P=`# C#\r
+\r
+---\r
+\r
+[\`C#\`](https://en.wikipedia.org/wiki/C_Sharp_(programming_language)) is a general-purpose high-level programming language supporting multiple paradigms. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic,  object-oriented (class-based), and component-oriented programming disciplines.\r
+\r
+---\r
+\r
+## My skills:\r
+\r
+| Skill	| Level	| Description |\r
+| :---- | :---- | :---------- |\r
+|	|	| 	|\r
+`,s=n=>n,D=[s({name:"3D Software",slug:"3D"}),s({name:"Game Engines",slug:"gameEng"}),s({name:"Programming Languages",slug:"pro-lang"}),s({name:"Frameworks",slug:"framework"}),s({name:"Libraries",slug:"library"}),s({name:"Langauges",slug:"lang"}),s({name:"Databases",slug:"db"}),s({name:"ORMs",slug:"orm"}),s({name:"DevOps",slug:"devops"}),s({name:"Testing",slug:"test"}),s({name:"Dev Tools",slug:"devtools"}),s({name:"Markup & Style",slug:"markup-style"}),s({name:"Design",slug:"design"}),s({name:"Soft Skills",slug:"soft"})],i=n=>{const t=v(n,"category");return n.category&&(t.category=D.find(o=>o.slug===n.category)),t},u=[i({slug:"blender",color:"orange",description:f,logo:a.Blender,name:"Blender",category:"3D"}),i({slug:"substancePainter",color:"orange",description:y,logo:a.SubstancePainter,name:"Substance Painter",category:"3D"}),i({slug:"unity",color:"white",description:w,logo:a.Unity,name:"Unity",category:"gameEng"}),i({slug:"photonFusion",color:"white",description:b,logo:a.PhotonFusion,name:"Photon Fusion",category:"framework"}),i({slug:"photonQuantum",color:"white",description:k,logo:a.PhotonQuantum,name:"Photon Quantum",category:"framework"}),i({slug:"unreal",color:"black",description:S,logo:a.Unreal,name:"Unreal Engine",category:"gameEng"}),i({slug:"csharp",color:"blue",description:P,logo:a.Csharp,name:"C#",category:"pro-lang"})],B="Skills",x=(...n)=>u.filter(t=>n.includes(t.slug)),L=n=>{const t=[],o=[];return u.forEach(r=>{if(n.trim()&&!r.name.toLowerCase().includes(n.trim().toLowerCase()))return;if(!r.category){console.log(r.category),o.push(r);return}let l=t.find(d=>{var p;return d.category.slug===((p=r.category)==null?void 0:p.slug)});l||(l={items:[],category:r.category},t.push(l)),l.items.push(r)}),o.length!==0&&t.push({category:{name:"Others",slug:"others"},items:o}),t};export{a as A,F as a,x as b,L as g,u as i,B as t};
--- _app/immutable/entry/start.VXA1B0f3.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.S336-0qv.js";export{t as start};
